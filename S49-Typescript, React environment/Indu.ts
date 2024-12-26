a80ff3e68db5fed26fd41df811ecf5c66ee93b9f
Sravya-Yeruva <yeruvasravya359@gmail.com>
2024-12-26 13:17:56 +0530

    Add files via upload

---
+++ S49-Typescript, React environment/Indu.ts
@@ -0,0 +1,14 @@
+let TelMarks:number=100;
+let HinMarks:number=100;
+let EngMarks:number=90;
+let MathMarks:number=100;
+let SciMarks:number=80;
+let SocMarks:number=90;
+
+TelMarks=88;
+
+let CalculateResult=(a:number,b:number)=>{
+    console.log(a,b);
+
+}
+CalculateResult(10,20,);
---
+++ S49-Typescript, React environment/Sravya.js
@@ -0,0 +1,14 @@
+let TeluguMarks=80;
+let HindiMarks=90;
+let EnglishMarks=100;
+let MathsMarks=70;
+let ScienceMarks=90;
+let SocialMarks=100;
+
+TeluguMarks="hi";
+
+let Result=(a,b)=>{
+    console.log(a,b);
+}
+Result(10);
+Result(10,20,30);
